c7b9ea3ded55b17f5ef369149d6264db10575398
Brian Noppe <noppebrian@gmail.com>
2016-02-11 22:30:41 +0100

    refactored substrings method

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,6 @@
 def substrings(string, dictionary)
   hash = {}
-  dictionary.each{|word| string.downcase.scan(/#{word}/) do |match|
-      if hash.has_key?("#{match}")
-        hash["#{match}"] = hash["#{match}"] + 1
-      else
-        hash["#{match}"] = 1
-      end
-    end
-  }
+  dictionary.each{|word| string.downcase.scan(/#{word}/) {|match| hash.has_key?("#{match}") ? hash["#{match}"] = hash["#{match}"] + 1 : hash["#{match}"] = 1}}
   puts hash
 end
 
