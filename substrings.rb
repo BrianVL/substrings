91465112a1eb0798f5e2b8c46b2e761f6de18c5e
Brian Noppe <noppebrian@gmail.com>
2016-02-11 22:18:00 +0100

    working version which handles multiple words

--- substrings.rb
+++ substrings.rb
@@ -1,9 +1,16 @@
 def substrings(string, dictionary)
   hash = {}
-  dictionary.each{|word| string.scan(/#{word}/){|match| hash[:"#{match}"] = 1}}
+  dictionary.each{|word| string.downcase.scan(/#{word}/) do |match|
+      if hash.has_key?("#{match}")
+        hash["#{match}"] = hash["#{match}"] + 1
+      else
+        hash["#{match}"] = 1
+      end
+    end
+  }
   puts hash
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low", "own","part","partner","sit"]
 
-substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
